722a9696039420f2cc22aac1ea2a950c46a94068
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-13 10:28:43 +0200

    Sending and receiving orders over the internet

--- TakingBasicOrderDetails/CheckoutView.swift
+++ TakingBasicOrderDetails/CheckoutView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -24,15 +27,49 @@ struct CheckoutView: View {
                         .font(.title)
                     
                     Button("Place order") {
-                        // place the order
-                        
+                        self.placeOrder()
                     }
                 .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
     }
+    
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "http://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // handle resume here
+            
+            // Verify if there is no internet connection
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknows error")")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on the way!"
+                self.showingConfirmation = true
+            } else {
+                print("Inalid response from server")
+            }
+            
+        }.resume()
+    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
